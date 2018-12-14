e59c5d0e9af9306374f1b113ee9abb415682c602
langziwd <1033726809@qq.com>
2018-12-14 23:59:25 +0800

    add Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{                            //public：声明   class：创建一个类的意思    Helloworld：类的名称  {}：固定的格式，类的内容
+    public static void main(String[] args) {        //public static void main(String[] args){}:固定格式，表示主方法，java所有的方法都是从这里开始运行的，理解为一个声明主方法的语句
+        System.out.println("Hello World");          //System.out.println     System下面的out里面的println方法  打印文字的固定输出方式  自动换行输出  不换行输出把println改为print          
+    }
+}
